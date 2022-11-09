8062acaa223affabe49beb7d73aadd8a5cdec731
Talimul212 <islamtalimul0@gmail.com>
2022-11-10 01:04:41 +0600

    reviews flie create in mongodb

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next) {
-const authHeader=req.headers.authorication;
-if (!authHeader) {
-   return res.status(401).send({message:'unauthorized access'})
-}
-const token=authHeader.split(' ')[1];
-jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
-    if(err){
-        res.status(401).send({message:'unauthorized access'})
+    const authHeader = req.headers.authorication;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
     }
-    req.decoded=decoded;
-    next();
-})
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
 
 }
 
@@ -63,12 +63,12 @@ async function run() {
 
         // orders api
 
-        app.get('/orders',verifyJWT, async (req, res) => {
-           const decoded=req.decoded;
-           console.log("inside orders api",decoded);
-           if (decoded.email!==req.query.email) {
-            res.status(403).send({message:'unauthorized access'})
-           }
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+           
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' })
+            }
             let query = {};
             if (req.query.email) {
                 query = {
@@ -79,16 +79,16 @@ async function run() {
             const orders = await cursor.toArray();
             res.send(orders);
         })
-       
 
-        app.post('/orders',verifyJWT, async (req, res) => {
+
+        app.post('/orders', verifyJWT, async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         })
-       
 
-        app.patch('/orders/:id',verifyJWT, async (req, res) => {
+
+        app.patch('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const status = req.body.status;
             const query = { _id: ObjectId(id) };
@@ -101,7 +101,7 @@ async function run() {
             res.send(result);
         })
 
-        app.delete('/orders/:id',verifyJWT, async (req, res) => {
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
@@ -109,28 +109,42 @@ async function run() {
         })
 
         // Reviews part_____________________________________________
-        app.get('/reviews',verifyJWT, async (req, res) => {
-            const decoded=req.decoded;
-            console.log("inside orders api",decoded);
-            if (decoded.email!==req.query.email) {
-             res.status(403).send({message:'unauthorized access'})
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+          
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' })
             }
-             let query = {};
-             if (req.query.email) {
-                 query = {
-                     email: req.query.email
-                 }
-             }
-             const cursor = reviewsCollection.find(query);
-             const orders = await cursor.toArray();
-             res.send(orders);
-         })
-         app.post('/reviews',verifyJWT, async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewsCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+        app.post('/reviews', verifyJWT, async (req, res) => {
             const order = req.body;
             const result = await reviewsCollection.insertOne(order);
             res.send(result);
         })
-        app.delete('/reviews/:id',verifyJWT, async (req, res) => {
+
+        app.patch('/reviews/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await reviewsCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
+        app.delete('/reviews/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await reviewsCollection.deleteOne(query);
